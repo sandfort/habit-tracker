40df5a907ab888c6370524531537424c7d317c7f
John Sandfort <john@jsand.us>
2020-12-29 15:48:57 -0500

    you can save a task record now

--- welcome.php
+++ welcome.php
@@ -1,7 +1,25 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'webby');
+define('DB_PASSWORD', 'honk');
+define('DB_NAME', 'habit_tracker');
+
+global $db;
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$query = "insert into task (name, date) values (\"" . $_POST["task"] . "\", \"" . $_POST["date"] . "\");";
+
+global $result;
+$result = mysqli_query($db, $query);
+?>
+
 <!DOCTYPE html>
 <html>
     <body>
-	Task: <?php echo $_POST["task"]; ?><br>
-	Date: <?php echo $_POST["date"]; ?><br>
+	<?php if ($result) { ?>
+	    Inserted with ID: <?php echo mysqli_insert_id($db) ?>
+	<?php } else { ?>
+	    Failed: <?php echo mysqli_error($db) ?>
+	<?php } ?>
     </body>
 </html>
