1d5f0605a6b3257b146aabbb09b9d54be65870aa
John Sandfort <john@jsand.us>
2020-12-29 12:54:24 -0500

    idk what i did in this commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+	<form action="welcome.php" method="post">
+	    Task: <input type="text" name="task"><br>
+            Date: <input type="date" name="date"><br>
+	    <input type="submit">
+	</form>
+    </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <body>
+	Task: <?php echo $_POST["task"]; ?><br>
+	Date: <?php echo $_POST["date"]; ?><br>
+    </body>
+</html>
