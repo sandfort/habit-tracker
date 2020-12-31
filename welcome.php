8269d505c158b7303e043fdd90e96bc3e8eea210
John Sandfort <john@jsand.us>
2020-12-30 21:27:35 -0500

    shows you the last time you did the thing

--- welcome.php
+++ welcome.php
@@ -5,7 +5,9 @@ $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 $result = mysqli_query($db, "insert into task (name, date) values (\"" . $_POST["task"] . "\", \"" . $_POST["date"] . "\");");
 
 if ($result) {
-    $rows = mysqli_query($db, "select date from task where name = \"" . $_POST["task"] . "\";");
+    $result = mysqli_query($db, "select date from task where name = \"" . $_POST["task"] . "\" order by date desc limit 1;");
+    $row = mysqli_fetch_row($result);
+    $last_date = $row[0];
 } else {
     $error = mysqli_error($db);
 }
@@ -16,12 +18,7 @@ if ($result) {
     <body>
 	<?php if ($result) { ?>
 	    Task: <?=$_POST["task"]?><br>
-	    This task was performed on:
-	    <ul>
-		<?php while ($row = mysqli_fetch_row($rows)) { ?>
-		    <li><?=$row[0]?></li>
-		<?php } ?>
-	    </ul>
+	    This task was last performed on: <?=$last_date?><br>
 	<?php } else { ?>
 	    Failed: <?=$error?>
 	<?php } ?>
